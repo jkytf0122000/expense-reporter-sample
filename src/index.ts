d0f65815ab7d3559d8bb46e4f3f14ec780157711
sho kisaragi <sho@oshiire.to>
2020-05-12 08:22:55 +0900

    change quotation

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,71 @@
-import Express from 'express';
+import Express from "express";
 const app = Express();
-import logger from 'morgan';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import { Authentication } from './controllers/auth/index';
+import logger from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { Authentication } from "./controllers/auth/index";
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 24 * 30 * 60 * 1000
-  }
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 30 * 60 * 1000,
+    },
+  })
+);
 
 Authentication.initialize(app);
 Authentication.setStrategy();
 
 // ログインの強制
 app.use((req, res, next) => {
-  if (req.isAuthenticated())
-    return next();
-  if (req.url === '/' || req.url === '/login')
-    return next();
-  res.redirect('/login');
+  if (req.isAuthenticated()) return next();
+  if (req.url === "/" || req.url === "/login") return next();
+  res.redirect("/login");
 });
 
 // ルーティング
-import index from './routes/index';
-import login from './routes/login';
-import payment from './routes/expenses/payment';
-import submit from './routes/expenses/submit';
+import index from "./routes/index";
+import login from "./routes/login";
+import payment from "./routes/expenses/payment";
+import submit from "./routes/expenses/submit";
 
-app.use('/', index);
-app.use('/expenses/payment', payment);
-app.use('/login', login);
-app.use('/expenses/submit', submit);
+app.use("/", index);
+app.use("/expenses/payment", payment);
+app.use("/login", login);
+app.use("/expenses/submit", submit);
 
-app.use((req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-  var err: any = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(
+  (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+    var err: any = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  }
+);
 
 // error handler
-app.use((err: any, req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(
+  (
+    err: any,
+    req: Express.Request,
+    res: Express.Response,
+    next: Express.NextFunction
+  ) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send('エラーが発生しました');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.send("エラーが発生しました");
+  }
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
