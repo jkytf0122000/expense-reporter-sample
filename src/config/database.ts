07f85568edb87e44b90a719bee6dc9d55013d56a
sho kisaragi <sho@oshiire.to>
2021-12-15 18:34:39 +0900

    add ssl option for postgres

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,8 +6,26 @@ export default function (): Sequelize {
 
   if (config.use_env_variable) {
     const config_url: any = process.env[config.use_env_variable];
-    return new Sequelize(config_url, config);
+    return new Sequelize(config_url, {
+      timezone: '+09:00',  // JST タイムゾーン : Sequelize で SELECT すると全て UTC の ISO 形式になっており DB 上の記録と異なる
+      logging: false,  // ログ出力
+      // SSL 接続のため指定する (↓ 以下を追加した)
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: true,
+        rejectUnauthorized: false
+      }
+    });
   } else {
-    return new Sequelize(config.database, config.username, config.password, config);
+    return new Sequelize(config.database, config.username, config.password, {
+      timezone: '+09:00',  // JST タイムゾーン : Sequelize で SELECT すると全て UTC の ISO 形式になっており DB 上の記録と異なる
+      logging: false,  // ログ出力
+      // SSL 接続のため指定する (↓ 以下を追加した)
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: true,
+        rejectUnauthorized: false
+      }
+    });
   }
 }
